5061c6b6ee15bb4d6e84930023272fdb050d595a
Arthur Sugden <arthur.sugden@gmail.com>
2018-08-02 12:41:09 -0400

    Improved backing up and loading

--- +pipe/load.m
+++ +pipe/load.m
@@ -1,30 +1,32 @@
 function out = load(mouse, date, run, ftype, server)
 % LOAD loads any type of important file of the scanbox format. Uses path
+% Can be passed a single input, the path to a file, which it will attempt
+% to parse
 
-    if nargin < 4
-        error('Call with mouse, date, run, and type of file.');
-    elseif nargin < 5
-        server = [];
-    end
-    
+    path = [];
     out = [];
-    path = pipe.lab.datapath(mouse, date, run, ftype, server);
     
-    % Share the results
-    if isempty(path)
-        error('File not found');
+    if nargin == 1
+        path = mouse;
+        [~, ~, ftype] = fileparts(path);
     end
+    if nargin < 5, server = []; end
+    
+    if isempty(path), path = pipe.lab.datapath(mouse, date, run, ftype, server); end
+    if isempty(path), error('File not found'); end
     
     % Read the necessary filetypes differently
     switch ftype
         case 'bhv'
-            out = pipe.io.bhvRead(path);
+            out = pipe.io.readBhv(path);
         case 'ephys'
             out = pipe.io.sbxEphys(mouse, date, run, server);
         case 'info'
             out = pipe.info(path);
         case 'onsets'
             out = pipe.io.sbxOnsets(mouse, date, run, server);
+        case 'tif'
+            out = pipe.io.readTiff(path);
         otherwise
             if ~exist(path, 'file')
                 error('File not found');
