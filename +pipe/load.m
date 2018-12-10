d3aba62b4ab3e24fe4a86141e5588873161f26ba
Arthur Sugden <arthur.sugden@gmail.com>
2018-12-10 16:20:01 -0500

    Made returning an error on pipe.load optional.

--- +pipe/load.m
+++ +pipe/load.m
@@ -1,7 +1,27 @@
-function out = load(mouse, date, run, ftype, server)
+function out = load(mouse, date, run, ftype, server, varargin)
 % LOAD loads any type of important file of the scanbox format. Uses path
 % Can be passed a single input, the path to a file, which it will attempt
 % to parse
+
+    p = inputParser;
+    % ---------------------------------------------------------------------
+    % Most important variables
+    addOptional(p, 'error', true);       % Raise error if the file doesn't exist if true
+    
+    if length(varargin) == 1 && iscell(varargin{1}), varargin = varargin{1}; end
+    parse(p, varargin{:});
+    p = p.Results;
+    
+    % Set the default values for mouse, date, and run
+    if isnumeric(date), date = num2str(date); end
+    if nargin < 3, run = pipe.lab.runs(mouse, date, p.server); end
+    
+    % Iterate over every run
+    if length(run) > 1
+        for r = run
+            pipe.io.simpcell(mouse, date, r, p);
+        end
+    end
 
     path = [];
     out = [];
@@ -13,7 +33,13 @@ function out = load(mouse, date, run, ftype, server)
     if nargin < 5, server = []; end
     
     if isempty(path), path = pipe.lab.datapath(mouse, date, run, ftype, server); end
-    if isempty(path), error('File not found'); end
+    if isempty(path)
+        if p.error
+            error('File not found');
+        else
+            return;
+        end
+    end
     
     % Read the necessary filetypes differently
     switch ftype
