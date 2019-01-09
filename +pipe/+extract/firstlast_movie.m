d3f2c9b5359696aa63d587bb73539fd45bd0bfbf
Jeff Zaremba <jzaremba@gmail.com>
2019-01-09 16:22:06 -0500

    Fix for firstlast movie to correctly handle single planes

--- +pipe/+extract/firstlast_movie.m
+++ +pipe/+extract/firstlast_movie.m
@@ -4,7 +4,7 @@ function firstlast_movie(path, n, pmt, varargin)
     p = inputParser;
     addOptional(p, 'type', 'sbx');  %sbx file type to open: sbx, sbxreg, xyreg, sbxclean, demonsreg
     addOptional(p, 'server', []);  % Server name
-    addOptional(p, 'startframe', 0, @isnumeric);
+    addOptional(p, 'startframe', 1, @isnumeric);
     addOptional(p, 'optolevel', []);
     addOptional(p, 'estimate', false);  % Whether to give an estimate of where the path would be if it does not exist
     parse(p, varargin{:});
@@ -12,14 +12,19 @@ function firstlast_movie(path, n, pmt, varargin)
     
     if nargin < 2, n = 500; end
     if nargin < 3, pmt = 1; end
+    info = pipe.metadata(path);
+    if isempty(p.optolevel)
+        last_start = info.nframes - n + 1;
+    else
+        last_start = info.nframes - n*length(info.otwave) + p.optolevel;
+    end
+      
     
     spath = sprintf('%s_first-last-%i.tif', path(1:strfind(path,'.')-1), n);
     
     if ~exist(spath)
-        info = pipe.metadata(path);
-
         f500 = pipe.imread(path, p.startframe, n, pmt, p.optolevel, 'register', true);
-        l500 = pipe.imread(path, info.max_idx + 1 - n, n, pmt, p.optolevel, 'register', true);
+        l500 = pipe.imread(path, last_start, n, pmt, p.optolevel, 'register', true);
 
         pipe.io.write_tiff(cat(3, f500, l500), spath, class(f500));
     end
