512314f0cf9bab92a044ab79fbc5f6494fc7f2c7
Jeff Zaremba <jzaremba@gmail.com>
2019-01-10 10:05:21 -0500

    Fix for multi-plane data Added optional arguments for forcing overwrite and registering movie.

--- +pipe/+extract/firstlast_movie.m
+++ +pipe/+extract/firstlast_movie.m
@@ -2,11 +2,11 @@ function firstlast_movie(path, n, pmt, varargin)
 %SBXFIRSTLAST Make TIFFs of the first and last 500 frames
 
     p = inputParser;
-    addOptional(p, 'type', 'sbx');  %sbx file type to open: sbx, sbxreg, xyreg, sbxclean, demonsreg
     addOptional(p, 'server', []);  % Server name
     addOptional(p, 'startframe', 1, @isnumeric);
     addOptional(p, 'optolevel', []);
-    addOptional(p, 'estimate', false);  % Whether to give an estimate of where the path would be if it does not exist
+    addOptional(p, 'force', false);
+    addOptional(p, 'register', true');
     parse(p, varargin{:});
     p = p.Results;
     
@@ -16,15 +16,15 @@ function firstlast_movie(path, n, pmt, varargin)
     if isempty(p.optolevel)
         last_start = info.nframes - n + 1;
     else
-        last_start = info.nframes - n*length(info.otwave) + p.optolevel;
+        last_start = floor(info.nframes/length(info.otwave)) - n + 1;
     end
       
     
     spath = sprintf('%s_first-last-%i.tif', path(1:strfind(path,'.')-1), n);
     
-    if ~exist(spath)
-        f500 = pipe.imread(path, p.startframe, n, pmt, p.optolevel, 'register', true);
-        l500 = pipe.imread(path, last_start, n, pmt, p.optolevel, 'register', true);
+    if p.force || ~exist(spath, 'file')
+        f500 = pipe.imread(path, p.startframe, n, pmt, p.optolevel, 'register', p.register);
+        l500 = pipe.imread(path, last_start, n, pmt, p.optolevel, 'register', p.register);
 
         pipe.io.write_tiff(cat(3, f500, l500), spath, class(f500));
     end
