09ef236c0a71614d5773c9c8075770d64e1466e4
Arthur Sugden <arthur.sugden@gmail.com>
2019-09-03 12:38:40 -0400

    Added 1-dimensional binning.

--- +pipe/+proc/bint.m
+++ +pipe/+proc/bint.m
@@ -4,16 +4,26 @@ function out = bint(data, bin)
     if nargin < 2, out = data; return; end
     if bin < 2, out = data; return; end
 
-    if bin > size(data, 3)
-        out = mean(data, 3);
+    if bin > size(data, ndims(data))
+        out = mean(data, ndims(data));
         return;
     end
     
-    [y, x, t] = size(data);
-    t = floor(t/bin)*bin;
-    out = data(:, :, 1:t);
-    
-    out = reshape(out, y*x, t)';
-    out = mean(reshape(out, bin, []), 1);
-    out = shiftdim(reshape(out, t/bin, y, x), 1);
+    if ndims(data) == 3
+        [y, x, t] = size(data);
+        t = floor(t/bin)*bin;
+        out = data(:, :, 1:t);
+
+        out = reshape(out, y*x, t)';
+        out = mean(reshape(out, bin, []), 1);
+        out = shiftdim(reshape(out, t/bin, y, x), 1);
+    elseif ismatrix(data)
+        [y, t] = size(data);
+        t = floor(t/bin)*bin;
+        out = data(:, 1:t);
+
+        out = reshape(out, y, t)';
+        out = mean(reshape(out, bin, []), 1);
+        out = shiftdim(reshape(out, t/bin, y), 1);
+    end
 end
